2c5910c2fc3c50827b1bd5dae5ee0d130b1fbf80
asml <p80@80q.ru>
2023-12-29 01:29:32 +0300

    +

--- src/request/chat/chat.controller.ts
+++ src/request/chat/chat.controller.ts
@@ -31,7 +31,7 @@ export class ChatController {
 	}
 
 	@Get("tgGetFilePhoto/:unic_id")
-	@Header("Content-type", "image/jpeg") 
+	// @Header("Content-type", "image/jpeg") 
 	tgGetFilePhoto(@Param("unic_id") unic_id: string) {
 		return this.chatService.tgGetFilePhoto(unic_id)
 	}
